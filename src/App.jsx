4e6343425cd7c6e28ab8f7b97e4ea4f05faa1d0a
Ajit33 <justajit33@gmail.com>
2023-10-20 04:14:03 +0530

    debugg done

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,42 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import './App.css'
+import { ThemeProvider } from './context/Theme'
+import Themebtn from './component/Themebtn'
+import Card from './component/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
+const [thememode,setThemeMode]=useState("light")
+const lightTheme=()=>{
+  setThemeMode("light")
+}
+const darkTheme=()=>{
+  setThemeMode("dark")
+}
 
+
+//actual change in theme
+
+useEffect(()=>{
+  document.querySelector('html').classList.remove("light","dark")
+  document.querySelector('html').classList.add(thememode)
+},[thememode])
   return (
-    <div className="flex flex-wrap min-h-screen items-center">
+
+  <ThemeProvider value={{thememode,darkTheme,lightTheme}}>
+       <div className="flex flex-wrap min-h-screen items-center">
     <div className="w-full">
         <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            
+            <Themebtn />
         </div>
 
         <div className="w-full max-w-sm mx-auto">
-           
+           <Card />
         </div>
     </div>
 </div>
+  </ThemeProvider>
   )
 }
 
